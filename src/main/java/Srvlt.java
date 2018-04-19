b50cc0cb745b448ca39453dcebe051f859c41bcc
h@di <hadi@hadisafari.ir>
2018-04-19 19:58:16 +0430

    switch to servlet

---
+++ src/main/java/Srvlt.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class Srvlt extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("<h1>Hello World</h1>");
+    }
+}
